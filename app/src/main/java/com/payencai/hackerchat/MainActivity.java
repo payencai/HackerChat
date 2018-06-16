9bd1598020721af9ca56e7b6a5e95eb28aa7c5b8
Lister Cai <2295038285@qq.com>
2018-06-16 22:57:20 +0800

    添加部分工具类

--- app/src/main/java/com/payencai/hackerchat/MainActivity.java
+++ app/src/main/java/com/payencai/hackerchat/MainActivity.java
@@ -3,11 +3,14 @@ package com.payencai.hackerchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.payencai.library.util.ToastUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToastUtil.showToast(this,"hello world");
     }
 }

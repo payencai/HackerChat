49e938af2383f01609b33f9d8e3df5e39cad1013
Lister Cai <2295038285@qq.com>
2018-06-22 16:36:12 +0800

    get请求封装

--- app/src/main/java/com/payencai/hackerchat/MainActivity.java
+++ app/src/main/java/com/payencai/hackerchat/MainActivity.java
@@ -2,15 +2,38 @@ package com.payencai.hackerchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.payencai.library.http.task.HttpCallback;
+import com.payencai.library.http.task.HttpTask;
+import com.payencai.library.http.task.KeyValue;
 import com.payencai.library.util.ToastUtil;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    String url="http://www.weather.com.cn/data/sk/101110101.html";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ToastUtil.showToast(this,"hello world");
+        doHttpRequest();
+    }
+    private void doHttpRequest() {
+        List<KeyValue> keyValues=new ArrayList<>();
+        Object header=null;
+        HttpTask.getInstance().doget(url, header, keyValues, new HttpCallback<UserInfo>() {
+            @Override
+            public void onSuccess(UserInfo result) {
+               //Log.e("name",result.getWeatherInfo().getCity())  ;
+            }
+
+            @Override
+            public void onError(Exception error) {
+
+            }
+        });
     }
 }
